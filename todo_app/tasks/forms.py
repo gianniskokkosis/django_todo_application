d19371d4f366e66dd0e94b6a4bdcd063f8a4e035
gianniskokkosis <t8160190@aueb.gr>
2020-10-19 11:33:26 +0300

    Final commit

--- todo_app/tasks/forms.py
+++ todo_app/tasks/forms.py
@@ -5,7 +5,7 @@ from .models import TaskItem
 
 class TaskItemForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'size':'70', 'class':'mr-3 ml-3'}))
-    status_completed = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class':'mr-3 ml-3'}))
+    status_completed = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class':'mr-3 ml-3'}))
 
     class Meta:
         model = TaskItem
